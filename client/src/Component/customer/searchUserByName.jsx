5d22401e13dd622f0ed3f64e8c4e40294b0655b8
Shubham Vishwakarma <skv6621@gmail.com>
2024-11-14 22:02:47 +0530

    added resposiveness

--- client/src/Component/customer/searchUserByName.jsx
+++ client/src/Component/customer/searchUserByName.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import './searchUserByName.css';
+import './searchUserbyName.css';
 
 const CustomerSearch = ({ onSelectCustomer }) => {
   const [query, setQuery] = useState("");
